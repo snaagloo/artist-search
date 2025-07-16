61fa22955b4c6f7e11fecb4c53bf910fb447e61b
snaag <snaag.dev@gmail.com>
2025-07-17 02:05:04 +0900

    feat: add response dto

---
+++ src/main/java/com/example/demo/dto/Response.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+//import lombok.Getter;
+//import lombok.Setter;
+
+//@Getter
+//@Setter
+public class Response {
+    public String artist_name;
+    public String demographic;
+    public String majorsongs;
+    public String related_artists;
+
+}
