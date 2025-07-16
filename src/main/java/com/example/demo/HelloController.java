c26be3dfbb1aab177916901902cbcff0d4a53de4
snaag <snaag.dev@gmail.com>
2025-07-17 02:05:18 +0900

    feat: return response using dto

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.dto.Response;
 import com.example.demo.service.ManiaDBClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -21,7 +24,7 @@ public class HelloController {
     }
 
     @GetMapping("/artist")
-    public String getArtist(@RequestParam String name) {
+    public List<Response> getArtist(@RequestParam String name) {
         return maniaDBClientService.getArtistData(name);
     }
 }
--- src/main/java/com/example/demo/service/ManiaDBClientService.java
+++ src/main/java/com/example/demo/service/ManiaDBClientService.java
@@ -2,14 +2,18 @@ package com.example.demo.service;
 
 import com.example.demo.dto.Item;
 import com.example.demo.dto.Member;
+import com.example.demo.dto.Response;
 import com.example.demo.dto.Rss;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.Array;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ManiaDBClientService {
@@ -19,7 +23,7 @@ public class ManiaDBClientService {
         this.webClient = webClient;
     }
 
-    public String getArtistData(String artistName) {
+    public List<Response> getArtistData(String artistName) {
         String baseUrl = "http://www.maniadb.com/api/search/";
         String suffix = "/?sr=artist&display=10&key=example&v=0.5";
         String encodedArtist = URLEncoder.encode(artistName, StandardCharsets.UTF_8);
@@ -31,16 +35,28 @@ public class ManiaDBClientService {
                 .bodyToMono(String.class)
                 .block();
 
+        List<Response> responseList = new ArrayList<>();
+
         try {
             XmlMapper mapper = new XmlMapper();
             Rss rss = mapper.readValue(xml, Rss.class);
 
+
             for (Item item : rss.channel.item) {
+                Response response = new Response();
                 System.out.println("🎤 아티스트 이름: " + item.title);
+                response.artist_name = item.title;
+
                 System.out.println("👥 구성: " + item.demographic);
+                response.demographic = item.demographic;
+
                 System.out.println("🎧 대표곡 리스트: " + item.maniadb_majorsonglist);
-                System.out.println("👥 멤버:");
+                response.majorsongs = item.maniadb_majorsonglist;
+
+                System.out.println("👥 관련 아티스트들:");
                 System.out.println(item.maniadb_relatedartistlist);
+                response.related_artists = item.maniadb_relatedartistlist;
+
 //                System.out.println(item.maniadb_majormembers);
 //                if (item.maniadb_majormembers != null) {
 //                    for (Member m : item.maniadb_majormembers) {
@@ -48,6 +64,8 @@ public class ManiaDBClientService {
 //                    }
 //                }
                 System.out.println("---------------");
+
+                responseList.add(response);
             }
 
         } catch (Exception e) {
@@ -55,7 +73,7 @@ public class ManiaDBClientService {
         }
 
         System.out.println(xml);
-        return xml;
-
+//        return xml;
+        return responseList;
     }
 }
